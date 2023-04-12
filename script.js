6661c113f4362edea8cb4187d572b7d4f764079d
HAERANG <abcrang@gmail.com>
2023-04-12 16:33:53 +0900

    230412_1633

--- script.js
+++ script.js
@@ -15,19 +15,103 @@ let score = {
 
 const questions = [
   {
-    image: 'https://example.com/question1.jpg',
+    image: 'https://picsum.photos/200/300?random=1',
     question: '문항 1: 이것보다 저것이 더 좋다는 것에 동의하십니까?',
     choices: ['예', '아니오'],
     score: ['N', 'S']
   },
   {
-    image: 'https://example.com/question2.jpg',
-    question: '문항 2: 아침에 일어나는 것이 어려우십니까?',
+    image: 'https://picsum.photos/200/300?random=2',
+    question: '문항 2: 휴대폰을 자주 사용하십니까?',
     choices: ['예', '아니오'],
-    score: ['I', 'E']
+    score: ['E', 'I']
   },
-  // 총 16개의 문항이 있어야 합니다.
+  {
+    image: 'https://picsum.photos/200/300?random=3',
+    question: '문항 3: 일을 미루지 않고 바로 처리하는 편입니까?',
+    choices: ['예', '아니오'],
+    score: ['J', 'P']
+  },
+  {
+    image: 'https://picsum.photos/200/300?random=4',
+    question: '문항 4: 누군가가 말을 할 때 집중해서 듣는 편입니까?',
+    choices: ['예', '아니오'],
+    score: ['S', 'N']
+  },
+  {
+    image: 'https://picsum.photos/200/300?random=5',
+    question: '문항 5: 타인과 대화할 때 자신의 생각을 잘 표현할 수 있는 편입니까?',
+    choices: ['예', '아니오'],
+    score: ['T', 'F']
+  },
+  {
+    image: 'https://picsum.photos/200/300?random=6',
+    question: '문항 6: 새로운 사람을 만날 때 친구가 되려고 노력하십니까?',
+    choices: ['예', '아니오'],
+    score: ['E', 'I']
+  },
+  {
+    image: 'https://picsum.photos/200/300?random=7',
+    question: '문항 7: 결정을 내릴 때 감정보다는 논리를 중요시하는 편입니까?',
+    choices: ['예', '아니오'],
+    score: ['T', 'F']
+  },
+  {
+    image: 'https://picsum.photos/200/300?random=8',
+    question: '문항 8: 실용성보다는 이론과 개념을 중요시하는 편입니까?',
+    choices: ['예', '아니오'],
+    score: ['N', 'S']
+  },
+  {
+    image: 'https://picsum.photos/200/300?random=9',
+    question: '문항 9: 주로 외향적인 성격을 가지고 있습니까?',
+    choices: ['예', '아니오'],
+    score: ['E', 'I']
+  },
+  {
+    image: 'https://picsum.photos/200/300?random=10',
+    question: '문항 10: 의견 차이가 있을 때 갈등을 피하는 편입니까?',
+    choices: ['예', '아니오'],
+    score: ['J', 'P']
+    },
+    {
+    image: 'https://picsum.photos/200/300?random=11',
+    question: '문항 11: 문제를 해결하는데 시간이 오래 걸려도 인내심을 가지고 차근차근 해결하는 편입니까?',
+    choices: ['예', '아니오'],
+    score: ['J', 'P']
+    },
+    {
+    image: 'https://picsum.photos/200/300?random=12',
+    question: '문항 12: 자신의 감정을 솔직하게 표현하는 편입니까?',
+    choices: ['예', '아니오'],
+    score: ['F', 'T']
+    },
+    {
+    image: 'https://picsum.photos/200/300?random=13',
+    question: '문항 13: 새로운 상황에 대처하는 데 유연하게 대처하는 편입니까?',
+    choices: ['예', '아니오'],
+    score: ['P', 'J']
+    },
+    {
+    image: 'https://picsum.photos/200/300?random=14',
+    question: '문항 14: 상대방의 의견을 경청하며 이해하려고 노력하는 편입니까?',
+    choices: ['예', '아니오'],
+    score: ['F', 'T']
+    },
+    {
+    image: 'https://picsum.photos/200/300?random=15',
+    question: '문항 15: 여러 가지 일을 동시에 처리하는 것을 좋아하는 편입니까?',
+    choices: ['예', '아니오'],
+    score: ['S', 'N']
+    },
+    {
+    image: 'https://picsum.photos/200/300?random=16',
+    question: '문항 16: 다른 사람들의 주의를 받는 것을 좋아하는 편입니까?',
+    choices: ['예', '아니오'],
+    score: ['E', 'I']
+    }
 ];
+
 
 function loadQuestion() {
   const q = questions[currentQuestion];
